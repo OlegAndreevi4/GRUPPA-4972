7f7be047be4874d95cc8e31868cc2408ee3cb670
Gat <ivanzzzo@gmail.com>
2023-09-17 19:39:28 +0300

    The tenth task is completed

--- Sem2Task10/Program.cs
+++ Sem2Task10/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//№10 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+//int firstDigit = number / 10
+
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()??"0");
+if(num > 100 && num < 1000)
+{
+    int SecondDigit = (num % 100)/10;
+    Console.WriteLine("Вторая цифра числа " + num + " это: " + SecondDigit ); 
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}
