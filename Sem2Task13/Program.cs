cd973d5e5ce0463b0ed7d173a2b62fd05f3fe718
Gat <ivanzzzo@gmail.com>
2023-09-17 22:38:36 +0300

    Task 15 completed

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -1,13 +1,13 @@
 ﻿// №13 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Введите число:");
-int num = int.Parse(Console.ReadLine()??"0");
-if(num > 100 && num < 1000)
-{
-    int lastDigit = num % 10;
-    Console.WriteLine("Последняя цифра числа " + num + " это: " + lastDigit ); 
-}
-else
-{
-    Console.WriteLine("Число не трёхзначное");
-} 
+// Console.WriteLine("Введите число:");
+// int num = int.Parse(Console.ReadLine()??"0");
+// if(num > 100 && num < 1000)
+// {
+//     int lastDigit = num % 10;
+//     Console.WriteLine("Последняя цифра числа " + num + " это: " + lastDigit ); 
+// }
+// else
+// {
+//     Console.WriteLine("Число не трёхзначное");
+// } 
---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,26 @@
+﻿// №15 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите номер:");
+int day = int.Parse(Console.ReadLine() ?? "0");
+if (day > 7)
+{
+    Console.WriteLine("Не является днём недели");
+}
+else
+{
+    string[] dayOfWeek = new string[7];
+
+    dayOfWeek[0] = "Понедельник";
+    dayOfWeek[1] = "Вторник";
+    dayOfWeek[2] = "Среда";
+    dayOfWeek[3] = "Четверг";
+    dayOfWeek[4] = "Пятница";
+    dayOfWeek[5] = "Суббота";
+    dayOfWeek[6] = "Воскресенье";
+
+Console.Write(dayOfWeek[day - 1]);
+}
+if (day == 6 || day == 7)
+{
+    Console.WriteLine(" - это выходной");
+}
