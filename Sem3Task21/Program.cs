ccb2e9a244495da3ad5a4d0f6d882d86727264b7
Gat <ivanzzzo@gmail.com>
2023-09-20 18:24:22 +0300

    Task 19 solved

--- Sem3Task21/Program.cs
+++ Sem3Task21/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №21: Программа принимает координаты двух точек и находит расстояние между ними в 3D пространстве.
+
