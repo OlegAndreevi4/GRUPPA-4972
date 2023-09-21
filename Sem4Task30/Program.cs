b380ee6ccb0fcd54ce1f6dadba7fceaf2b313e5a
Gat <ivanzzzo@gmail.com>
2023-09-21 23:03:19 +0300

    30 task solved.

---
+++ Sem4Task30/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача № 30. Программа выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+//Ввод длины массива
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+//Метод генерации массива
+int[] GenArr(int len)
+{
+    int[] arr = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < len; i++)
+        {
+            arr[i] = rnd.Next(0, 2);
+        }
+    return arr;
+}
+
+//Вывод массива
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++) 
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int num = ReadData("Ввведите длину массива: ");
+int[] res = GenArr(num);
+PrintArray(res);
