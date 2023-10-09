00681f6a6dbd9be9a250088c4a952676b2f766b6
Gat <ivanzzzo@gmail.com>
2023-10-09 03:18:05 +0300

    Full set

--- Sem9Task66/Program.cs
+++ Sem9Task66/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Ввод данных
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Сумма натуральных чисел
+int SumNaturalNum(int m, int n)
+{
+  if (m == n) return m;
+  else return m + SumNaturalNum(m + 1, n);
+}
+
+int m = ReadData("Введите m: ");
+int n = ReadData("Введите n: ");
+int sum = SumNaturalNum(m, n);
+Console.WriteLine("Сумма натуральных элементов равна " + sum);
--- Sem9Task68/Program.cs
+++ Sem9Task68/Program.cs
@@ -1,1 +1,26 @@
-﻿// 
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// Ввод данных
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Функция Аккермана
+static int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else
+      if ((m != 0) && (n == 0))
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = ReadData("Введите m: ");
+int n = ReadData("Введите n: ");
+int res = Akkerman(m, n);
+Console.WriteLine("Функция Аккермана равна " + res);
